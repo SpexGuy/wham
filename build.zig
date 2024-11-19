48f45de35ab3849b7591a62c37d08a3bf61ac167
Martin Wickham <spexguy070@gmail.com>
2024-11-19 12:14:46 -0800

    Update build script

--- build.zig
+++ build.zig
@@ -1,29 +1,30 @@
 const std = @import("std");
-const mach = @import("mach/build.zig");
-const build_zmath = @import("mach/examples/libs/zmath/build.zig");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const app = mach.App.init(b, .{
+    const exe = b.addExecutable(.{
         .name = "wham",
-        .src = "src/main.zig",
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
-        .deps = &[_]std.build.Pkg{
-            .{ .name = "zmath", .source = .{ .path = "mach/examples/libs/zmath/src/zmath.zig" } },
-        },
-    }) catch unreachable;
-    app.setBuildMode(mode);
-    app.link(.{}) catch unreachable;
-    app.install();
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
 
-    const run_cmd = app.run() catch unreachable;
-    run_cmd.dependOn(b.getInstallStep());
-    // if (b.args) |args| {
-    //     run_cmd.addArgs(args);
-    // }
+    // Add Mach to our library and executable
+    const mach_dep = b.dependency("mach", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("mach", mach_dep.module("mach"));
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 
     const run_step = b.step("run", "Run the app");
-    run_step.dependOn(run_cmd);
+    run_step.dependOn(&run_cmd.step);
 }
